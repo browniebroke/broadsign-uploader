916c4a1fa96ee6b80084c2a4c31f55b68beefa0a
Bruno Alla <bruno.alla@festicket.com>
2019-08-16 19:34:34 +0200

    Site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Broadsign uploader`,
+    description: `Automate uploading to broadsign from an URL.`,
+    author: `@browniebroke`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
