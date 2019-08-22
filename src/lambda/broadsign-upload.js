e6b57f5dfdbe27155e62e6c31aac888690657de1
Bruno Alla <bruno.alla@festicket.com>
2019-08-22 12:06:11 +0100

    Try to fix Lambda function

--- src/lambda/broadsign-upload.js
+++ src/lambda/broadsign-upload.js
@@ -1,26 +1,43 @@
 import axios from "axios"
 
 export async function handler(event, context, callback) {
-  console.log("Handle form submit")
+  console.log("Handler starting")
+  console.log(event.body)
+
+  const bodyObj = JSON.parse(event.body)
+  console.log(bodyObj)
 
   const data = {
-    "url": event.body.contentUrl,
-    "name": event.body.title,
-    "domain_id": event.body.domainId,
+    url: bodyObj.contentUrl,
+    name: bodyObj.title,
+    domain_id: bodyObj.domainId,
   }
   console.log(data)
-  // Make the request
-  const response = await axios.post(
-    "https://api-sandbox.broadsign.com:10889/rest/content/v11/import_from_url",
-    data,
-    {
-      headers: {
-        "Accept": "application/json",
-        "Authorization": `Bearer ${event.body.token}`,
-        "Content-Type": "application/json;charset=utf-8",
-      },
-    },
-  )
-  console.log(response)
-  return await response.json()
+  try {
+    // Make the request
+    const response = await axios.post(
+      "https://api-sandbox.broadsign.com:10889/rest/content/v11/import_from_url",
+      data,
+      {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${bodyObj.token}`,
+          "Content-Type": "application/json;charset=utf-8",
+        },
+      }
+    )
+    console.log(response)
+    const data = response.data
+    console.log(data)
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    }
+  } catch (err) {
+    console.log(err) // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }),
+    }
+  }
 }
